911a1d50115f52cbdc9e073a2e9fd98df1e04187
J-reed <46486685+J-reed@users.noreply.github.com>
2023-02-13 08:29:20 +0000

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Justin Reed on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Budget
+//
+//  Created by Justin Reed on 13/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
